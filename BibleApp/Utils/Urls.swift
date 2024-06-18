11f2925b63364aba469091b4533df3fbb315dd16
Lawrence Gimenez <law@gmnz.xyz>
2024-06-18 16:22:58 +0800

    Utils Api for getting bibles

---
+++ BibleApp/Utils/Urls.swift
@@ -0,0 +1,15 @@
+//
+//  Urls.swift
+//  BibleApp
+//
+//  Created by Lawrence Gimenez on 6/18/24.
+//
+
+class Urls {
+    
+    static let baseAPI = "https://api.scripture.api.bible/v1/"
+    
+    public enum Api {
+        static let bibles = "bibles"
+    }
+}
