6c15ad6d67fbb5244f94d8591710a3454a1bee17
Lawrence Gimenez <law@gmnz.xyz>
2024-06-18 17:53:06 +0800

    Forced language=eng in bibles

--- BibleApp/Utils/Urls.swift
+++ BibleApp/Utils/Urls.swift
@@ -11,7 +11,7 @@ class Urls {
     static let apiKey = "ef4a0868a00fde6ad3b3e8cd756022c4"
     
     public enum Api {
-        static let bibles = "\(baseAPI)/bibles"
+        static let bibles = "\(baseAPI)/bibles?language=eng"
         static let books = "\(baseAPI)/bibles/%@/books"
         static let chapters = "\(baseAPI)/bibles/%@/chapters/%@"
     }
