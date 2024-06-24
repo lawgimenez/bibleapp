c862f716884796d5f4dfa61e28e0e7877c43ef07
Lawrence Gimenez <law@gmnz.xyz>
2024-06-24 08:14:47 +0800

    Initial WIP home page

--- BibleApp/Main/HomeView.swift
+++ BibleApp/Main/HomeView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    enum Pages: String {
+        case bible
+        case highlights
+        case notes
+        case profile
+    }
+    
+    @State private var selectedTab: Pages = .bible
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selectedTab) {
+            BibleView()
+                .tabItem {
+                    Label(Pages.bible.rawValue.capitalized, systemImage: "book")
+                }
+                .tag(Pages.bible)
+        }
     }
 }
 
