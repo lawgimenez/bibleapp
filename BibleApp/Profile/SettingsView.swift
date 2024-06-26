f031beb1fe0307e2a9ce8bc26e67d5c3171fb225
Lawrence Gimenez <law@gmnz.xyz>
2024-06-26 16:05:54 +0800

    Sign out functionality

--- BibleApp/Profile/SettingsView.swift
+++ BibleApp/Profile/SettingsView.swift
@@ -9,10 +9,43 @@ import SwiftUI
 
 struct SettingsView: View {
     
+    @Environment(\.modelContext) private var modelContext
+    @EnvironmentObject private var authObservable: AuthObservable
+    @State private var isSignOutAlertShown = false
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                
+            List {
+                Section(header: Text("Account")) {
+                    Text("Sign Out")
+                        .alert("Sign Out", isPresented: $isSignOutAlertShown) {
+                            Button(role: .destructive) {
+                                do {
+                                    try modelContext.delete(model: Item.self)
+                                    try modelContext.delete(model: BibleData.self)
+                                    try modelContext.delete(model: BookData.self)
+                                    try modelContext.delete(model: ChapterData.self)
+                                    try modelContext.delete(model: PassageData.self)
+                                    try modelContext.delete(model: Highlight.self)
+                                } catch {
+                                    print("SwiftData model delete error: \(error)")
+                                }
+                                if let bundleIdentifier = Bundle.main.bundleIdentifier {
+                                    UserDefaults.standard.removePersistentDomain(forName: bundleIdentifier)
+                                    UserDefaults.resetStandardUserDefaults()
+                                    UserDefaults.standard.synchronize()
+                                }
+                                authObservable.signInStatus = .loggedOut
+                            } label: {
+                                Text("Sign Out")
+                            }
+                            Button(role: .cancel) {
+
+                            } label: {
+                                Text("Cancel")
+                            }
+                        }
+                }
             }
         }
     }
