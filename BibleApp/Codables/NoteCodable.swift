5b1f7f7db2f7e2b23ece0ce9931d7c20db003115
Lawrence Gimenez <law@gmnz.xyz>
2024-07-03 17:51:14 +0800

    Initial note codable for saving to API

---
+++ BibleApp/Codables/NoteCodable.swift
@@ -0,0 +1,35 @@
+//
+//  NoteCodable.swift
+//  BibleApp
+//
+//  Created by Lawrence Gimenez on 7/3/24.
+//
+
+import Foundation
+
+struct NoteCodable: Encodable {
+    
+    let passage: String
+    let userNote: String
+    let color: String
+    let length: Int
+    let location: Int
+    let bibleId: String
+    let bibleName: String
+    let chapterId: String
+    let chapterName: String
+    let userUuid: String
+
+    enum CodingKeys: String, CodingKey {
+        case passage
+        case userNote = "user_note"
+        case color
+        case length
+        case location
+        case bibleId = "bible_id"
+        case bibleName = "bible_name"
+        case chapterId = "chapter_id"
+        case chapterName = "chapter_name"
+        case userUuid = "user_uuid"
+    }
+}
