54bc58fd55c290c3554cda517d108fafd3db4846
Lawrence Gimenez <law@gmnz.xyz>
2024-07-07 16:18:20 +0800

    Code refactor

--- BibleApp/Bible/PassageView.swift
+++ BibleApp/Bible/PassageView.swift
@@ -167,12 +167,12 @@ struct PassageView: View {
             mutableString = NSMutableAttributedString(string: trimmed)
             mutableString.replaceCharacters(in: note.getRange(), with: noteMutable)
             // Add highlights for notes
-            let highlightAttributes: [NSAttributedString.Key: Any] = [
+            let highlightNoteAttributes: [NSAttributedString.Key: Any] = [
                 .backgroundColor: UIColor(Color(.highlightPink)),
                 .font: UIFont.preferredFont(forTextStyle: UIFont.TextStyle.body),
                 .attachment: noteAttachment
             ]
-            mutableString.addAttributes(highlightAttributes, range: note.getRange())
+            mutableString.addAttributes(highlightNoteAttributes, range: note.getRange())
         }
         for highlight in highlights {
             // Add highlights
@@ -266,20 +266,20 @@ extension UIColor {
     }
 
     convenience init(hexString: String) {
-            let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-            var int = UInt64()
-            Scanner(string: hex).scanHexInt64(&int)
-            let a, r, g, b: UInt64
-            switch hex.count {
-            case 3: // RGB (12-bit)
-                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-            case 6: // RGB (24-bit)
-                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-            case 8: // ARGB (32-bit)
-                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-            default:
-                (a, r, g, b) = (255, 0, 0, 0)
-            }
-            self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
         }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
 }
